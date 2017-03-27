34234be40d553faee283666abd05182ffa7fbd9e
Rob Alonzi <alonzi.rob@gmail.com>
2017-03-27 13:28:22 -0400

    Added inital component

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,8 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 var { Route, Router, IndexRoute, hashHistory } = require("react-router");
 
+var TodoApp = require("./components/TodoApp.jsx");
+
 
 //Load foundation
 require("style-loader!css-loader!foundation-sites/dist/css/foundation.min.css");
@@ -11,6 +13,6 @@ $(document).foundation();
 require("style-loader!css-loader!sass-loader!./styles/app.scss");
 
 ReactDOM.render(
-	<p>Boilerplate3 Project</p>,
+	<TodoApp />,
 	document.getElementById('app')
 );
---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+var React = require("react");
+
+
+var TodoApp = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<h1>ToDoApp.jsx</h1>
+			</div>
+		);
+	}
+});
+
+module.exports = TodoApp;
