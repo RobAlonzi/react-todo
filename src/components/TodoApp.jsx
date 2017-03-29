10928cbdd235728a3b3cb611cee71237ff0acff3
Rob Alonzi <alonzi.rob@gmail.com>
2017-03-29 16:29:10 -0400

    Add toggle support

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -14,19 +14,23 @@ var TodoApp = React.createClass({
 			todos: [
 				{
 					id: uuid(),
-					text: 'Walk the dog'
+					text: 'Walk the dog',
+					completed: false
 				},
 				{
 					id: uuid(),
-					text: 'Clean the yard'
+					text: 'Clean the yard',
+					completed: true
 				},
 				{
 					id: uuid(),
-					text: 'Clean the dishes'
+					text: 'Clean the dishes',
+					completed: true
 				},
 				{
 					id: uuid(),
-					text: 'Go to the gym'
+					text: 'Go to the gym',
+					completed: false
 				}
 			]
 		}
@@ -34,8 +38,27 @@ var TodoApp = React.createClass({
 
 	handleAddTodo: function(text){
 		this.setState({
-			todos: [...this.state.todos, { id: uuid(), text: text }]
+			todos: [
+				...this.state.todos, 
+				{ 
+					id: uuid(), 
+					text: text,
+					completed: false
+			 	}
+			]
 		});
+	},
+
+	handleToggle: function(id){
+		var updatedTodos = this.state.todos.map((todo) => {
+			if(todo.id === id){
+				todo.completed = !todo.completed;
+			}
+
+			return todo;
+		});
+
+		this.setState({todos: updatedTodos})
 	},
 
 	handleSearch: function(showCompleted, searchText){
@@ -51,7 +74,7 @@ var TodoApp = React.createClass({
 			<div>
 				<h1>ToDoApp.jsx</h1>
 				<TodoSearch onSearch={this.handleSearch} />
-				<TodoList todos={todos} />
+				<TodoList todos={todos} onToggle={this.handleToggle}/>
 				<TodoForm onAddTodo={this.handleAddTodo} />
 			</div>
 		);
--- src/tests/components/Todo.test.jsx
+++ src/tests/components/Todo.test.jsx
@@ -9,4 +9,21 @@ describe("Todo", () => {
 	it("should exist", () => {
 		expect(Todo).toExist();
 	});
+
+	it("should call on onToggle prop with id on click", () => {
+		var todoData = {
+			id: 199,
+			text: "write todo.tst.jsx test",
+			completed: true
+		};
+
+		var spy = expect.createSpy();
+		var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy} />);
+		var el = ReactDOM.findDOMNode(todo);
+
+		console.log(el);
+
+		TestUtils.Simulate.click(el);
+		expect(spy).toHaveBeenCalledWith(199);
+	});
 })
